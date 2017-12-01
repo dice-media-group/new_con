b083362503b78b92bf1f14897e4888b6d2cd7e54
captproton <carl@wdwhub.net>
2017-11-30 19:17:23 -0800

    FIXED: typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,7 @@ ActionMailer::Base.smtp_settings = {
  :user_name => ENV['SENDGRID_USERNAME'],
  :password => ENV['SENDGRID_PASSWORD'],
  :domain => 'heroku.com',
- :address => 'smtp.sendgrid.net'
- ,
+ :address => 'smtp.sendgrid.net',
  :port => 587,
  :authentication => :plain,
  :enable_starttls_auto => true
