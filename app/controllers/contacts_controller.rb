14aa797fc1a652f992b9d771e7e746116cb2deda
captproton <carl@wdwhub.net>
2017-11-30 19:55:33 -0800

    CHANGED: ContactForm to Contact and added working form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,18 @@
 class ContactsController < ApplicationController
-	def new
-		@contact = ContactForm.new
-	end
+
+	def new 
+	 @contact = Contact.new
+	 end
 
 	def create
-		@contact = ContactForm.new(params[:contact])
-		@contact.request = request
-		if @contact.deliver
-			flash.now[:error] = nil
-		else
-			flash.now[:error] = 'Cannot send message.'
-			render :new
-		end
+	 @contact = Contact.new(params[:contact])
+	 @contact.request = request
+	 if @contact.deliver
+	 	flash.now[:error] = nil
+	 	redirect_to root_path, notice: 'Message sent successfully'
+	 else
+	 	flash.now[:error] = 'Cannot send message'
+	 	render :new
+	 end
 	end
 end
